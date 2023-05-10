7e37e6b2e154e18f87189054cca2b221479671e8
Diego ROJAS <rojasdiegopro@gmail.com>
2023-05-10 14:18:32 +0800

    Experiment with go/scanner and go/ast to improve tokenization

---
+++ samples/comments.go
@@ -0,0 +1,17 @@
+// Hello
+// World
+package main
+
+/* Goodbye */ import (
+	"fmt"
+)
+
+type Foo struct {
+	// Bar
+	Bar int// Baz
+	Beez string // Buz
+}
+
+func main() {
+	fmt.Println("Hello, World!" /* Sneaky */)
+}
---
+++ samples/hello.go
@@ -0,0 +1,8 @@
+// Hello World Program
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
