85e7372bb3631dea6ed45728a522dca74568b8ca
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2023-05-12 02:38:42 +0800

    Add file loading functionality to demo

--- demo/editor.py
+++ demo/editor.py
@@ -15,6 +15,19 @@ topFrame.pack(fill=tk.X)
 code.pack(fill=tk.BOTH, expand=True)
 label.pack(side='left')
 
-# label.config(text='sup bro')
+def handle_button_click(event):
+    file_to_load = entry.get()
+    if file_to_load.strip() == '':
+        label.config(text='Enter a file path', foreground='red')
+        return
+    try:
+        with open(file_to_load, 'r') as f:
+            code.delete('1.0', tk.END)
+            code.insert('1.0', f.read())
+        label.config(text=f'Loaded "{file_to_load}" successfully', foreground='black')
+    except:
+        label.config(text=f'Error when opening "{file_to_load}"', foreground='red')
+
+button.bind('<Button-1>', handle_button_click)
 
 window.mainloop()
