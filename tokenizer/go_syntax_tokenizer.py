a0b295749c1c46e176dfa9528e493577e22a7c05
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2023-05-12 00:58:18 +0800

    Zip go tokenizer output

--- tokenizer/go_syntax_tokenizer.py
+++ tokenizer/go_syntax_tokenizer.py
@@ -1,12 +1,16 @@
 import sys
 import os
+import json
+from pdb import set_trace as st
 
-def tokenize(filename):
+def tokenize_file(filename):
     output = os.popen(f'go run tokenizer/scanner/scanner.go < {filename}').read()
+    output = json.loads(output)
+    output = list(zip(output['token_ids'], output['token_names'], output['token_values']))
     return output
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Specify a file to tokenize.')
     else:
-        print(tokenize(sys.argv[1]))
+        print(tokenize_file(sys.argv[1]))
