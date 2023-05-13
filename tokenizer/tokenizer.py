14d9e202c4bccce148ee62b02d2d255a4151a548
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2023-05-13 19:45:20 +0800

    Add goscannertokenizer

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -10,11 +10,8 @@ from tokenizers import Tokenizer as WrappedHFTokenizer
 from tokenizers.trainers import BpeTrainer, Trainer as HFTrainer
 from pickle import load, dump
 from dataclasses import dataclass
-import json
-import sys
+import functools
 from typing import Any, List
-import os
-import ctypes
 
 class Tokenizer(ABC):
     @abstractmethod
@@ -34,7 +31,7 @@ class Tokenizer(ABC):
         pass
 
     @abstractmethod
-    def token_to_id(self, token: str) -> int:
+    def special_token_to_id(self, token: str) -> int:
         pass
 
     @abstractmethod
@@ -69,7 +66,7 @@ class HFTokenizer(Tokenizer):
     def train_from_iterator(self, iterator: Iterable):
         return self.tokenizer.train_from_iterator(iterator, self.trainer)
 
-    def token_to_id(self, token: str) -> int:
+    def special_token_to_id(self, token: str) -> int:
         return self.tokenizer.token_to_id(token)
 
     def id_to_token(self, id: int) -> str:
@@ -101,9 +98,7 @@ class GoScannerTokenizer(Tokenizer):
 
         self.BASIC_TYPES_START = self.NUM_GO_TOKENS
         self.EXTRA_TOKENS_START = self.BASIC_TYPES_START + len(self.BASIC_TYPES)
-        self.SPECIAL_TOKENS_START = self.EXTRA_TOKENS_START + len(self.EXTRA_TOKENS)
-        self.BPE_START = self.SPECIAL_TOKENS_START + len(special_tokens)
-
+        self.BPE_START = self.EXTRA_TOKENS_START + len(self.EXTRA_TOKENS)
 
     def _get_string_literal(self, string):
         return string[1:-1]
@@ -125,7 +120,6 @@ class GoScannerTokenizer(Tokenizer):
 
     def encode(self, sequence: str) -> List[int]:
         go_scanner_results = go_scanner_scan(sequence)
-        
         tokens = list(
             map(
                 lambda tok_tuple: ('IDENT_TYPE', tok_tuple[1], tok_tuple[2]) if tok_tuple[1] in self.BASIC_TYPES else tok_tuple,
@@ -138,15 +132,15 @@ class GoScannerTokenizer(Tokenizer):
         for token in tokens:
             if token[0] == 'STRING':
                 new_tokens.append(self.EXTRA_TOKENS_START + self.EXTRA_TOKENS.index('"'))
-                new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1][1:-1]).ids[1:]]
+                new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1][1:-1]).ids]
                 new_tokens.append(self.EXTRA_TOKENS_START + self.EXTRA_TOKENS.index('"'))
             elif token[0] == 'COMMENT':
                 if token[1][:2] == '//':
                     new_tokens.append(self.EXTRA_TOKENS_START + self.EXTRA_TOKENS.index('//'))
-                    new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1][2:]).ids[1:]]
+                    new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1][2:]).ids]
                 else:
                     new_tokens.append(self.EXTRA_TOKENS_START + self.EXTRA_TOKENS.index('/*'))
-                    new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1][2:-2]).ids[1:]]
+                    new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1][2:-2]).ids]
                     new_tokens.append(self.EXTRA_TOKENS_START + self.EXTRA_TOKENS.index('*/'))
             elif token[0] == 'IDENT':
                 new_tokens += [sf + self.BPE_START for sf in self.tokenizer.encode(token[1]).ids]
@@ -165,25 +159,31 @@ class GoScannerTokenizer(Tokenizer):
             return go_scanner_id_to_token_name(id)
         elif id < self.EXTRA_TOKENS_START:
             return self.BASIC_TYPES[id - self.BASIC_TYPES_START]
-        elif id < self.SPECIAL_TOKENS_START:
-            return self.EXTRA_TOKENS[id - self.EXTRA_TOKENS_START]
         elif id < self.BPE_START:
-            return self.special_tokens[id - self.SPECIAL_TOKENS_START]
+            return self.EXTRA_TOKENS[id - self.EXTRA_TOKENS_START]
         else:
             return self.tokenizer.decode([id - self.BPE_START])
     
-    def token_to_id(self, token: str) -> int:
+    def special_token_to_id(self, token: str) -> int:
         return self.tokenizer.token_to_id(token)
 
-    def train_from_iterator(self, iterator: Iterable):
+    def seq_to_bpe_tokens(self, seq):
+        go_scanner_results = go_scanner_scan(seq)
+        tokens = list(
+            map(
+                lambda tok_tuple: ('IDENT_TYPE', tok_tuple[1], tok_tuple[2]) if tok_tuple[1] in self.BASIC_TYPES else tok_tuple,
+                zip(go_scanner_results.names, go_scanner_results.literals, go_scanner_results.ids)
+            )
+        )
+        return [tok[1] for tok in tokens if tok[0] in ['IDENT', 'STRING', 'COMMENT']]
         
-        return self.tokenizer.train_from_iterator(iterator, self.trainer)
-
-
-
+    def train_from_iterator(self, iterator: Iterable):
+        bpe_token_set_gen = (self.seq_to_bpe_tokens(seq) for seq in iterator)
+        train_iter = functools.reduce(lambda a,b: a+b, bpe_token_set_gen)
+        self.tokenizer.train_from_iterator(train_iter, self.trainer)
 
     def get_vocab_size(self) -> int:
-        return self.tokenizer.get_vocab_size() + self.NUM_GO_TOKENS
+        return self.tokenizer.get_vocab_size() + self.BPE_START
 
     def save(self, path: str):
         self.tokenizer.save(path)
@@ -214,7 +214,7 @@ class GoAstTokenizer(Tokenizer):
     def train_from_iterator(self, iterator: Iterable):
         pass
 
-    def token_to_id(self, token: str) -> int:
+    def special_token_to_id(self, token: str) -> int:
         pass
 
     def id_to_token(self, id: int) -> str:
