700cb3ad2b6ab011bcb4e394c7722ac94ca7126b
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2023-05-13 16:11:53 +0800

    Update go_syntax_tokenizer.py to make use of new go tokenizer dll, and to integrate bpe-encoded tokens into go token list

--- tokenizer/go_syntax_tokenizer.py
+++ tokenizer/go_syntax_tokenizer.py
@@ -1,7 +1,10 @@
 import sys
-import os
-import json
 from pdb import set_trace as st
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Metaspace
+from tokenizers.processors import TemplateProcessing
 from tokenizer import go_scanner_scan
 
 BASIC_TYPES = set(['int', 'int8', 'int16 ', 'int32 ', 'int64', 'uint', 'uint8', 'uint16', 'uint32', 'uint64', 'uintptr', 'float32', 'float64', 'complex64', 'complex128', 'string', 'bool', 'byte', 'rune',])
@@ -30,44 +33,48 @@ def tokenize_file(filename):
     
 def tokenize_string(src):
     go_scanner_results = go_scanner_scan(src)
-    st()
-    token_json = json.loads(go_scanner_results)
     
     tokens = list(
         map(
             lambda tok_tuple: ('IDENT_TYPE', tok_tuple[1]) if tok_tuple[1] in BASIC_TYPES else tok_tuple,
-            zip(token_json['token_names'], token_json['token_values'])
+            zip(go_scanner_results.names, go_scanner_results.literals)
         )
     )
-    
+
     string_toks = get_tokens_of_type(tokens, 'STRING')
-    ident_toks = get_tokens_of_type(tokens, 'IDENT')
     comment_toks = get_tokens_of_type(tokens, 'COMMENT')
-    int_toks = get_tokens_of_type(tokens, 'INT')
-    float_toks = get_tokens_of_type(tokens, 'FLOAT')
 
     string_toks = list(map(get_string_literal, string_toks))
     comment_toks = list(map(get_comment_literal, comment_toks))
-    
-    combined_bpe_toks = string_toks + ident_toks + comment_toks + int_toks + float_toks
-    tok_file_text = '[SEP]'.join(combined_bpe_toks)
-    with open('temp_tokens', 'w') as f:
-        f.write(tok_file_text)
 
-    tokenizer = new_tokenizer()
-    train_tokenizer(tokenizer, ['temp_tokens'], 2**15)
-    # save_tokenizer(tokenizer, 'config/tokenizer_gs.json')
+    combined_bpe_toks = string_toks + comment_toks
+    
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+    tokenizer.pre_tokenizer = Metaspace() # type: ignore
+    trainer = BpeTrainer(vocab_size=2**15, special_tokens=["[UNK]", "[PAD]", "[CLS]", "[SEP]"]) # type: ignore
+    tokenizer.train_from_iterator(combined_bpe_toks, trainer)
+    tokenizer.post_processor = TemplateProcessing(
+        single="[CLS] $A",
+        special_tokens=[
+            ("[CLS]", tokenizer.token_to_id("[CLS]")),
+        ],
+    ) # type: ignore
 
     new_tokens = []
 
     for token in tokens:
         if token[0] == 'STRING':
-            # remove quotes then feed to tokenizer
-            pass
+            new_tokens.append(('STRING_START', token[1][0]))
+            new_tokens += [('STRING_FRAG', sf) for sf in tokenizer.encode(token[1][1:-1]).tokens[1:]]
+            new_tokens.append(('STRING_END', token[1][-1]))
         elif token[0] == 'COMMENT':
-            pass
-        elif token[0] in ['IDENT', 'INT', 'FLOAT']:
-            pass
+            if token[1][:2] == '//':
+                new_tokens.append(('INLINE_COMMENT_START', '//'))
+                new_tokens += [('COMMENT_FRAG', sf) for sf in tokenizer.encode(token[1][2:]).tokens[1:]]
+            else:
+                new_tokens.append(('BLOCK_COMMENT_START', '/*'))
+                new_tokens += [('COMMENT_FRAG', sf) for sf in tokenizer.encode(token[1][2:-2]).tokens[1:]]
+                new_tokens.append(('BLOCK_COMMENT_END', '*/'))
         else:
             new_tokens.append(token)
 
