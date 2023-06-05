cfa170e8c8926efa4ec76509c8df3922c94fb687
Diego ROJAS <rojasdiegopro@gmail.com>
2023-06-05 21:34:17 +0800

    Remove useless load samples script

--- load_sample.py
+++
@@ -1,9 +0,0 @@
-import numpy
-
-
-array = numpy.load("./.cache/datasets/the-stack-dedup-v1.2/hugging-face-pretokenized/shard-000.npy")
-
-print("Shape:", array.shape)
-print("Size:", array.nbytes / 1024 / 1024, "MB")
-print("Data type:", array.dtype)
-print("First 10 tokens:", array[:10])
